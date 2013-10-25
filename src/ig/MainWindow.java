38f00920a71f7050bf418605af11fe7db9a94dd0
MickaelFardilha <mickael.fardilha@gmail.com>
2013-10-25 05:22:16 -0700

    Init projet

---
+++ src/ig/MainWindow.java
@@ -0,0 +1,19 @@
+package ig;
+
+/**
+ * @author Mickael
+ *
+ */
+public class MainWindow
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
